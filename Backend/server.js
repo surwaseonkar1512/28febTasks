69e380b36d33f42e15979755d6e82bab05eacae4
surwaseonkar1512 <surwaseonkar12345@gmail.com>
2025-02-28 13:24:49 +0530

    "start"

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/orders", orderRoutes);
 
-app.use(errorHandler); // Error handling middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
